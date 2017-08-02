c950ba07f0c94331e7392f67996a44ac1ce8d590
viktorgino <vviktor2@gmail.com>
2017-08-02 21:25:29 +0100

    Remove unnecessary Mazda files

--- mazda/libunwind-1.2-rc1/build-libunwind.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#update the temp paths as required
-OUTDIR=/home/lucas/libunwindbuild
-M3TOOLCHAIN=/home/lucas/m3-toolchain-lmagder
-
-./configure --prefix=$OUTDIR --enable-static --disable-shared --with-sysroot=$M3TOOLCHAIN/arm-cortexa9_neon-linux-gnueabi/sysroot --target=arm-cortexa9_neon-linux-gnueabi --host=arm-cortexa9_neon-linux-gnueabi --enable-cxx-exceptions PATH=$PATH:$M3TOOLCHAIN/bin
-make install PATH=$PATH:$M3TOOLCHAIN/bin
-
